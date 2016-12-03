eab0d11a039d290bc8057fcc3f6c748a72e7b197
richardjboothe <richardjboothe@gmail.com>
2016-12-03 17:44:34 -0600

    first example of state

---
+++ Redux_by_Abramov/public/intro.js
@@ -0,0 +1,34 @@
+function counter(state, action) {
+  if (state === undefined) {
+    return 0;
+  }
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if(action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state;
+  }
+}
+
+expect(
+  counter(0, {type: 'INCREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, {type: 'INCREMENT'})
+).toEqual(2)
+
+expect(
+  counter(2, {type: 'DECREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, {type: 'DECREMENT'})
+).toEqual(0)
+
+expect(
+  counter(undefined, {})
+).toEqual(0)
+
+console.log('Tests passed!');
