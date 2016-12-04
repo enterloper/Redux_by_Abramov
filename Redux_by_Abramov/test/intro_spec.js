dfc0981e3742d25e693cbb2389e3dd1223fa6d62
richardjboothe <richardjboothe@gmail.com>
2016-12-04 00:29:19 -0600

    write tests for completed counter and incomplete todo list

--- Redux_by_Abramov/test/intro_spec.js
+++ Redux_by_Abramov/test/intro_spec.js
@@ -1,21 +1,64 @@
-expect(
-  counter(0, {type: 'INCREMENT'})
-).toEqual(1)
+const expect = require('expect');
+const deepFreeze = require('deep-freeze');
 
-expect(
-  counter(1, {type: 'INCREMENT'})
-).toEqual(2)
+const addCounter = (list) => {
+	// list.push(0); will not work due to deepFreeze
+	// return list.concat([0]); will work because it returns a completely new array.
+	return [...list, 0];
+};
 
-expect(
-  counter(2, {type: 'DECREMENT'})
-).toEqual(1)
+const removeCounter = (list, index) => {
+	return list
+		.slice(0,index)
+		.concat(list.slice(index+1));
+	return [
+		...list.slice(0, index),
+		...list.slice(index+1)
+	];
+};
 
-expect(
-  counter(1, {type: 'DECREMENT'})
-).toEqual(0)
+const incrementCounter = (list, index) => {
+	// return list
+	// 	.slice(0, index)
+	// 	.concat([list[index] + 1])
+	// 	.concat(list.slice(index + 1));
+	return [
+		...list.slice(0, index),
+		list[index] + 1,
+		...list.slice(index + 1)
+		]
+}
 
-expect(
-  counter(undefined, {})
-).toEqual(0)
+const testAddCounter = () => {
+	const listBefore = [];
+	const listAfter = [0];
+	deepFreeze(listBefore);
 
-console.log('Tests passed!');
+	expect(
+		addCounter(listBefore)
+	).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+	const listBefore = [0,10,20];
+	const listAfter = [0, 20];
+	deepFreeze(listBefore);
+
+	expect(
+		removeCounter(listBefore, 1)
+	).toEqual(listAfter);
+}
+
+const testIncrementCounter = () => {
+	const listBefore = [0,10,20];
+	const listAfter = [0,11,20];
+	deepFreeze(listBefore);
+	expect(
+		incrementCounter(listBefore,1)
+	).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log(`All Tests passed!`);
