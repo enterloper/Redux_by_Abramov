c225f14f7a519434b9096c0f6faecef71d282d48
richardjboothe <richardjboothe@gmail.com>
2016-12-03 18:24:16 -0600

    introduce store behavior getState, dispatch, and subscribe

--- Redux_by_Abramov/public/intro.js
+++ Redux_by_Abramov/public/intro.js
@@ -1,34 +1,53 @@
-function counter(state, action) {
-  if (state === undefined) {
-    return 0;
-  }
-  if (action.type === 'INCREMENT') {
-    return state + 1;
-  } else if(action.type === 'DECREMENT') {
-    return state - 1;
-  } else {
-    return state;
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+	    return state + 1;
+  	case 'DECREMENT':
+	    return state - 1;
+ 		default:
+	    return state;
   }
 }
 
-expect(
-  counter(0, {type: 'INCREMENT'})
-).toEqual(1)
+const { createStore } = Redux;
+/*
+THE ABOVE ON LINE 11 IS THE SAME AS THOSE ON 14 & 15
+var createStore = Redux.createStore;
+import { createStore } = from 'redux;
+*/
 
-expect(
-  counter(1, {type: 'INCREMENT'})
-).toEqual(2)
+/*
+This store binds together the three principles of Redux. 1: It holds the current application's state object. 2: It lets you dispatch actions. 3: When you create it, you need to specify the reducer that tells how state is updated with actions.
 
-expect(
-  counter(2, {type: 'DECREMENT'})
-).toEqual(1)
+In this example, we're calling createStore with "counter" as the reducer that manages the state updates. This store has three important methods.
+*/
+  const store = createStore(counter);
+  console.log(store.getState());
+/*
+The first method of this store is called getState. It retrieves the current state of the Redux store. If we were on this, we're going to see 0 because this is the initial state of our application.
 
-expect(
-  counter(1, {type: 'DECREMENT'})
-).toEqual(0)
+The second and the most commonly used store method is called dispatch. It lets you dispatch actions to change the state of your application. If we log this store state after dispatch, we're going to see that it has changed.
 
-expect(
-  counter(undefined, {})
-).toEqual(0)
+  store.dispatch({ type: 'INCREMENT' });
+  console.log(store.getState());
 
-console.log('Tests passed!');
+store.subscribe lets you register a callback that the Redux store will call ANY TIME AN ACTION HAS BEEN DISPATCHED, so that you can update the UI of your application. It will reflect the current application state.
+*/
+
+const render = () => {
+	document.body.innerText = store.getState();
+}
+
+store.subscribe(render)
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+})
+
+/*
+The rendering of below will not take place initially because it is called inside the subscribe function. For this reason we can extract the behavior into a function called render to get an initial rendering.
+store.subscribe(() => {
+   document.body.innerText = store.getState();
+});
+*/
