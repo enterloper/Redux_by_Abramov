4dc6a94408725690df64a21ecf0391e23d6ce07b
richardjboothe <richardjboothe@gmail.com>
2016-12-04 19:18:33 -0600

    Implement filter through Redux

--- Redux_by_Abramov/src/index2.js
+++ Redux_by_Abramov/src/index2.js
@@ -50,10 +50,40 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-let nextTodoId = 0;
+const FilterLink = ({
+	filter,
+	children
+}) => {
+	return (
+		<a href='#'
+			onClick={e => {
+				e.preventDefault();
+				store.dispatch({
+					type: 'SET_VISIBILITY_FILTER',
+					filter
+				});
+			}}
+		>
+			{children}
+		</a>
+	);
+};
 
+const getVisibleTodos = (todos, filter) => {
+	switch (filter) {
+		case 'SHOW_ALL':
+			return todos;
+		case 'SHOW_COMPLETED':
+			return todos.filter(t => t.completed)
+		case 'SHOW_ACTIVE':
+			return todos.filter(t => !t.completed)
+	}
+}
+
+let nextTodoId = 0;
 class TodoApp extends Component {
 	render() {
+		const visibleTodos = getVisibleTodos(this.props.todos, this.props.visibilityFilter);
 		return (
 			<div>
 				<input ref={node => {
@@ -70,7 +100,7 @@ class TodoApp extends Component {
 					Add Todo
 				</button>
 				<ul>
-					{this.props.todos.map(todo => (
+					{visibleTodos.map(todo => (
 						<li key={todo.id}
 								onClick={() => {
 									store.dispatch({
@@ -85,6 +115,27 @@ class TodoApp extends Component {
 						</li>
 					))}
 				</ul>
+				<p>
+					Show:
+					{' '}
+					<FilterLink
+						filter='SHOW_ALL'
+					>
+						All
+					</FilterLink>
+					{' '}
+					<FilterLink
+						filter='SHOW_ACTIVE'
+					>
+						Active
+					</FilterLink>
+					{' '}
+					<FilterLink
+						filter='SHOW_COMPLETED'
+					>
+						Completed
+					</FilterLink>
+				</p>
 			</div>
 		);
 	}
@@ -93,8 +144,8 @@ class TodoApp extends Component {
 const render = () => {
 	ReactDOM.render(
 		<TodoApp
-			todos={store.getState().todos} />,
-			document.getElementById('root')
+			{...store.getState()}
+		/>,document.getElementById('root')
 	);
 };
 
