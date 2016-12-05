eb7d18c60d0acc9a87d881b03428f6beba155cd7
richardjboothe <richardjboothe@gmail.com>
2016-12-04 23:31:32 -0600

    create action creators

--- Redux_by_Abramov/src/index2.js
+++ Redux_by_Abramov/src/index2.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider, connect } from 'react-redux';
 
+/************ ACTION CREATORS ************/
+let nextTodoId = 0;
+const addTodo = (text) => {
+	return {
+		type: 'ADD_TODO',
+		id: nextTodoId++,
+		text
+	};
+};
+
+const setVisibilityFilter = (filter) => {
+	return {
+		type: 'SET_VISIBILITY_FILTER',
+		filter
+	}
+}
+
+const toggleTodo = (id) => {
+	return {
+		type: 'TOGGLE_TODO',
+		id
+	};
+};
+
 const todo = (state, action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
@@ -78,10 +102,7 @@ const mapStateToLinkProps = (state, ownProps) => {
 const mapDispatchToLinkProps = (dispatch, ownProps) => {
 	return {
 		onClick: () => {
-			dispatch({
-				type: 'SET_VISIBILITY_FILTER',
-				filter: ownProps.filter
-			})
+			dispatch(setVisibilityFilter(ownProps.filter))
 		}
 	}
 }
@@ -138,11 +159,7 @@ let AddTodo = ({ dispatch }) => {
 					input = node;
 				}} />
 				<button onClick={() => {
-				dispatch({
-					type: 'ADD_TODO',
-					id: nextTodoId++,
-					text: input.value
-				})
+				dispatch(addTodo(input.value));
 				input.value = '';
 			}}>
 					Add Todo
@@ -174,10 +191,7 @@ const mapStateToTodoListProps = (state) => {
 const mapDispatchToTodoListProps = (dispatch) => {
 	return {
 		onTodoClick: (id) => {
-			dispatch({
-				type: 'TOGGLE_TODO',
-				id
-			})
+			dispatch(toggleTodo(id))
 		}
 	};
 };
@@ -186,7 +200,6 @@ const VisibleTodoList = connect(
 	mapDispatchToTodoListProps
 )(TodoList);
 
-let nextTodoId = 0;
 
 const TodoApp = () => (
 	<div>
